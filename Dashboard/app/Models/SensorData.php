f9ce4677507c602ac52f1df133fac1e2cc533d48
Jureen <jrnltvrs@gmail.com>
2026-08-11 10:41:48 +0800

    refactor: Remove SoftDeletes trait from SensorData model

--- Dashboard/app/Models/SensorData.php
+++ Dashboard/app/Models/SensorData.php
@@ -8,7 +8,7 @@ use Illuminate\Database\Eloquent\SoftDeletes;
 
 class SensorData extends Model
 {
-    use HasFactory, SoftDeletes;
+    use HasFactory;
 
     /**
      * The table associated with the model.
