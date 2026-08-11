7cdb5a1884b266ddf6181e82e44b0a58d5ada7f2
engrn <nasayaonimshi@gmail.com>
2026-08-11 16:41:15 +0800

    Change the icon

--- Dashboard/resources/views/layouts/header.blade.php
+++ Dashboard/resources/views/layouts/header.blade.php
@@ -5,6 +5,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta name="csrf-token" content="{{ csrf_token() }}">
     <title>{{ config('app.name', 'EMS Gateway') }}</title>
+    <link rel="icon" type="image/svg+xml" href="{{ asset('ems-icon.svg') }}">
     <script src="https://cdn.tailwindcss.com"></script>
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
