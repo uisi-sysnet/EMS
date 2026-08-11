d6fdc3a6df160e427489626a0a92c21c0212d584
Jureen <jrnltvrs@gmail.com>
2026-08-11 13:55:14 +0800

    feat: Add Alpine.js for enhanced interactivity in header

--- Dashboard/resources/views/layouts/header.blade.php
+++ Dashboard/resources/views/layouts/header.blade.php
@@ -10,6 +10,7 @@
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
     <link href="https://fonts.googleapis.com/css2?family=Roboto+Condensed:ital,wght@0,100..900;1,100..900&display=swap" rel="stylesheet">
     <script src="https://cdn.jsdelivr.net/npm/sweetalert2@11"></script>
+    <script defer src="https://cdn.jsdelivr.net/npm/alpinejs@3.x.x/dist/cdn.min.js"></script>
     <style>
         @import url('https://fonts.googleapis.com/css2?family=Inter:wght@400;500;600&family=JetBrains+Mono:wght@400;500&display=swap');
         * {
