b67b869f694086627654fe19f09d182094991ceb
Jureen <jrnltvrs@gmail.com>
2026-08-26 13:36:35 +0800

    refactor: Simplify SMS ingestion status display logic in service cards

--- Dashboard/resources/views/server/services.blade.php
+++ Dashboard/resources/views/server/services.blade.php
@@ -46,7 +46,7 @@
                     </div>
 
                     @if($svc['isSms'] ?? false)
-                        <p class="text-xs text-text-500 mt-2">SMS Ingestion: <span class="sms-status-text font-semibold {{ $svc['running'] ? 'text-munti-green-400' : 'text-munti-red-400' }}">{{ $svc['running'] ? 'True' : 'False' }}</span></p>
+                        <p class="text-xs text-text-500 mt-2">SMS Ingestion: <span class="sms-status-text font-semibold {{ $svc['running'] }}">{{ $svc['running'] ? 'True' : 'False' }}</span></p>
                         {{-- SMS-specific enable/disable buttons --}}
                         <div class="flex gap-2 mt-4">
                             <button type="button" 
